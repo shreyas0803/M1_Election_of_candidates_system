a431e024b5a3964ff8bb5089159d16054dfbdec6
shreyas0803 <101561890+shreyas0803@users.noreply.github.com>
2022-04-02 19:04:14 +0530

    Update Header.h

--- 3_Implementation/Header.h
+++ 3_Implementation/Header.h
@@ -1,1 +1,56 @@
+#include<stdio.h>
 
+#define CANDIDATE_COUNT
+#define CANDIDATE1 //candidate name1
+#define CANDIDATE2 //candidate name2
+#define CANDIDATE3 //candidate name3
+#define CANDIDATE4 //candidate name4
+
+void castvote();
+void votesCount();
+void getLeadingCandidate();
+
+void castVote(){
+int choice;
+/* enter all choices to get print
+*/
+
+switch(choice){
+    /* required choices
+    */
+}
+
+}
+void votesCount(){
+    /* required votescount  
+*/
+}
+
+void getLeadingCandidate(){
+    /* getting all leading candidates
+    */
+        
+}
+
+int main()
+{
+int i;
+int choice;
+
+do{
+    /* all options dispalyed
+    */
+
+
+switch(choice)
+{
+    /*switching commands
+    */
+}
+}while(choice!=0);
+
+//hold the screen
+getchar();
+
+return 0;
+}
